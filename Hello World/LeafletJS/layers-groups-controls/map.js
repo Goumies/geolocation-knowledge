23d852b8f1b16861ab23903faee1d004dbe9f557
Goumies <contact@romyalula.com>
2018-11-14 15:47:25 +0100

    Implements layer groups and layer controls

--- Hello World/LeafletJS/layers-groups-controls/map.js
+++ Hello World/LeafletJS/layers-groups-controls/map.js
@@ -1,9 +1,30 @@
-var myMap;
+var myMap, streets, cities;
+
+
+// Bunch of layers to combine in a group
+var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+
+// Adding them all as a group
+cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+var grayscale = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        id: 'mapbox.light',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        accessToken: 'pk.eyJ1IjoiZ291bWllcyIsImEiOiJjam9mdWQwdDMwM2FkM3JvM282N2Fjczk1In0.cn5judg7WkANSxSGTESEtA'
+    }),
+    streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        id: 'mapbox.streets',
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        accessToken: 'pk.eyJ1IjoiZ291bWllcyIsImEiOiJjam9mdWQwdDMwM2FkM3JvM282N2Fjczk1In0.cn5judg7WkANSxSGTESEtA'
+    });
 
 myMap = L.map('mapid', {
     center: [39.73, -104.99],
     zoom: 10,
-    layers: [streets, cities]
+    layers: [grayscale, cities]
 });
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -13,24 +34,18 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiZ291bWllcyIsImEiOiJjam9mdWQwdDMwM2FkM3JvM282N2Fjczk1In0.cn5judg7WkANSxSGTESEtA'
 }).addTo(myMap);
 
-// Bunch of layers to combine in a group
-var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
-    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
-    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
-    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+// Switching layers
+var baseMaps, overlayMaps;
 
-// Adding them all as a group
-var cities = L.layerGroup([littleton, denver, aurora, golden]).addTo(myMap);
+baseMaps = {
+    "<span style='color: gray'>Grayscale</span>": grayscale,
+    "Streets": streets
+};
 
-var grayscale = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        id: 'mapbox.grayscale',
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-    }),
-    streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        id: 'mapbox.streets',
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        accessToken: 'pk.eyJ1IjoiZ291bWllcyIsImEiOiJjam9mdWQwdDMwM2FkM3JvM282N2Fjczk1In0.cn5judg7WkANSxSGTESEtA'
-    });
+overlayMaps  = {
+    "Cities": cities
+};
 
-
+// Controls to switch
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
